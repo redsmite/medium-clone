f2bd94b3aad5b0425c2e9ef840ba957b3f56b650
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-06-14 16:19:46 +0800

    add flowbite tabs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Make sure you have users to assign to the posts!
-        User::factory(10)->create();
+        User::factory()->create([
+            "name"=> "John User",
+            'email' => 'test@example.com',
+        ]);
 
         $categories = [
             'Technology',
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,27 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+
+                    <ul class="flex flex-wrap text-sm font-medium text-center text-body justify-center">
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-2.5 text-white bg-blue-600 rounded-base active" aria-current="page">Tab 1</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">Tab 2</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">Tab 3</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">Tab 4</a>
+                        </li>
+                        <li>
+                            <a class="inline-block px-4 py-3 text-fg-disabled cursor-not-allowed">Tab 5</a>
+                        </li>
+                    </ul>
+
                 </div>
             </div>
         </div>
